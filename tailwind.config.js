638de74bd23c82974a7ef3812a923eb35782a233
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2024-01-23 21:08:20 -0300

    feat: add base styles and new animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            backgroundImage: {
+                home: 'url("/src/images/backgroundImage.png")',
+            },
+            dropShadow: {
+                '3xl': '5px 5px 15px rgba(0, 250, 0, 0.75)',
+            },
+            animation: {
+                blink: 'blink 1s linear infinite',
+            },
+            keyframes: {
+                blink: {
+                    '0%, 100%': { opacity: 0 },
+                    '25%, 75%': { opacity: 0.5 },
+                    '50%': { opacity: 1 },
+                },
+            },
+        },
     },
     plugins: [],
 }
