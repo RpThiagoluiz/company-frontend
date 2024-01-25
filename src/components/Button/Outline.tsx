b98d6eb7e8017e243429824e249de2c9f58565ba
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2024-01-25 18:47:32 -0300

    feat: button filled and refactor button component

--- src/components/Button/Outline.tsx
+++ src/components/Button/Outline.tsx
@@ -2,12 +2,14 @@ import { ButtonHTMLAttributes } from 'react'
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
     text: string
+    extraStyles?: React.ComponentProps<'button'>['className']
 }
 
-export function Outline({ text, ...rest }: Props) {
+export function Outline({ text, extraStyles, ...rest }: Props) {
     return (
         <button
-            className="transition ease-in-out delay-150 bg-transparent hover:bg-green-600 text-green-600 font-semibold hover:text-white py-2 px-4 mr-auto border border-green-600 hover:border-transparent rounded"
+            type="button"
+            className={`transition ease-in-out delay-150 bg-transparent hover:bg-green-600 text-green-600 font-semibold hover:text-white py-2 px-4 mr-auto border border-green-600 hover:border-transparent rounded ${extraStyles}`}
             {...rest}
         >
             {text}
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,5 +1,7 @@
 import { Outline } from './Outline'
+import { FilledGradient } from './FilledGradient'
 
 export const Button = {
     Outline,
+    FilledGradient,
 }
