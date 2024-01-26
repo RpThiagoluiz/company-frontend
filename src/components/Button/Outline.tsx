48ef90af55660ebbc835e114b64dc13ad80e56bc
Rpthiagoluiz <rp.thiagoluiz@gmail.com>
2024-01-26 18:03:13 -0300

    refactor: button props for children elements

--- src/components/Button/Outline.tsx
+++ src/components/Button/Outline.tsx
@@ -1,18 +1,18 @@
 import { ButtonHTMLAttributes } from 'react'
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
-  text: string
+  children: React.ReactNode
   extraStyles?: React.ComponentProps<'button'>['className']
 }
 
-export function Outline({ text, extraStyles, ...rest }: Props) {
+export function Outline({ children, extraStyles, ...rest }: Props) {
   return (
     <button
       type="button"
-      className={`transition ease-in-out delay-150 bg-transparent hover:bg-green-600 text-green-600 font-semibold hover:text-white py-2 px-4 mr-auto border border-green-600 hover:border-transparent rounded ${extraStyles}`}
+      className={`transition ease-in-out delay-150 bg-transparent hover:bg-green-600 text-green-600 font-semibold hover:text-white py-2 px-4 border border-green-600 hover:border-transparent rounded ${extraStyles}`}
       {...rest}
     >
-      {text}
+      {children}
     </button>
   )
 }
