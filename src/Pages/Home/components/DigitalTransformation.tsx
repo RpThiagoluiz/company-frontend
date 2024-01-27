010213f92959c293d2ce741c999d837474989c61
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2024-01-27 17:53:39 -0300

    refactor: digital transformation component in home page

--- src/Pages/Home/components/DigitalTransformation.tsx
+++ src/Pages/Home/components/DigitalTransformation.tsx
@@ -3,7 +3,46 @@ import { PiArrowFatLinesUpBold } from 'react-icons/pi'
 import { BsBarChartLine } from 'react-icons/bs'
 import { GrUserExpert } from 'react-icons/gr'
 import { useEffect, useRef } from 'react'
-import { useAnimation, useInView, motion } from 'framer-motion'
+import {
+  useAnimation,
+  useInView,
+  motion,
+  type AnimationControls,
+} from 'framer-motion'
+import { type IconType } from 'react-icons'
+
+const CARDS_INFO = [
+  {
+    title: 'Desenvolvimento de Site',
+    description:
+      'Criamos sites de alta qualidade, garantindo uma experiência responsiva excepcional para seus usuários.',
+    icon: TbWorldWww,
+  },
+  {
+    title: 'Desenvolvimento de Aplicativos',
+    description:
+      'Desenvolvemos soluções móveis eficientes e abrangentes. Em todas as plataformas, iOS e Android.',
+    icon: TbDeviceMobileUp,
+  },
+  {
+    title: 'UX/UI Design',
+    description:
+      'Contamos com uma equipe especializada para criar designs envolventes e intuitivos, aprimorando a experiência do usuário.',
+    icon: GrUserExpert,
+  },
+  {
+    title: 'Trafego Pago',
+    description:
+      'Potencializamos a visibilidade online da sua empresa com estratégias de tráfego pago, utilizando ferramentas de ponta como Google Ads, Instagram e Facebook ads para impulsionar seu crescimento.',
+    icon: PiArrowFatLinesUpBold,
+  },
+  {
+    title: 'Trafego Orgânico',
+    description:
+      'Otimizamos sua presença online com estratégias de tráfego orgânico, fortalecendo a visibilidade e a reputação da sua empresa.',
+    icon: BsBarChartLine,
+  },
+]
 
 export function DigitalTransformation() {
   const headingRef = useRef<HTMLHeadingElement | null>(null)
@@ -18,140 +57,59 @@ export function DigitalTransformation() {
   }, [isInView, motionAnimation])
 
   return (
-    <article className="p-8 md:p-24  min-h-screen">
+    <article className="p-8 md:p-24 m-20 bg-zinc-700 rounded-2xl">
       <motion.h1
         ref={headingRef}
-        className="text-3xl md:text-5xl font-bold md:ml-5 text-center mb-28"
+        className="text-3xl text-white md:text-5xl font-bold md:ml-5 text-center mb-28"
       >
         Transforme
       </motion.h1>
       <ul className="grid grid-cols-1 gap-x-6 gap-y-16 md:grid-cols-2 xl:grid-cols-3">
-        <motion.li
-          ref={liRef}
-          variants={{
-            hidden: { opacity: 0, scale: 0.25 },
-            visible: { opacity: 1, scale: 1 },
-          }}
-          initial="hidden"
-          aria-hidden="true"
-          animate={motionAnimation}
-          transition={{ duration: 0.75, delay: 0.5 }}
-          className="group flex flex-col justify-around rounded-xl  p-5 min-h-[20rem] bg-slate-800"
-        >
-          <div className="flex items-center gap-7">
-            <TbWorldWww className="text-6xl text-green-500 drop-shadow-3xl group-hover:scale-125 transition ease-in-out delay-150" />
-            <h3 className="text-2xl text-white strong font-semibold">
-              Desenvolvimento de Site
-            </h3>
-          </div>
-
-          <p className="mt-10 text-white">
-            Criamos sites de alta qualidade, garantindo uma experiência
-            responsiva excepcional para seus usuários.
-          </p>
-        </motion.li>
-
-        <motion.li
-          ref={liRef}
-          variants={{
-            hidden: { opacity: 0, scale: 0.25 },
-            visible: { opacity: 1, scale: 1 },
-          }}
-          initial="hidden"
-          aria-hidden="true"
-          animate={motionAnimation}
-          transition={{ duration: 0.75, delay: 0.5 }}
-          className="group flex flex-col justify-around rounded-xl bg-slate-800  p-5 min-h-[20rem]"
-        >
-          <div className="flex items-center gap-7">
-            <TbDeviceMobileUp className="text-6xl text-green-500 drop-shadow-3xl group-hover:scale-125 transition ease-in-out delay-150" />
-            <h3 className="text-2xl text-white strong font-semibold">
-              Desenvolvimento de Aplicativos
-            </h3>
-          </div>
-
-          <p className="mt-10 text-white">
-            Desenvolvemos soluções móveis eficientes e abrangentes. Em todas as
-            plataformas, iOS e Android.
-          </p>
-        </motion.li>
-
-        <motion.li
-          ref={liRef}
-          variants={{
-            hidden: { opacity: 0, scale: 0.25 },
-            visible: { opacity: 1, scale: 1 },
-          }}
-          initial="hidden"
-          aria-hidden="true"
-          animate={motionAnimation}
-          transition={{ duration: 0.75, delay: 0.5 }}
-          className="group flex flex-col justify-around rounded-xl bg-slate-800  p-5 min-h-[20rem]"
-        >
-          <div className="flex items-center gap-7">
-            <GrUserExpert className="text-6xl text-green-500 drop-shadow-3xl group-hover:scale-125 transition ease-in-out delay-150" />
-            <h3 className="text-2xl text-white strong font-semibold">
-              UX/UI Design
-            </h3>
-          </div>
-
-          <p className="mt-10 text-white">
-            Contamos com uma equipe especializada para criar designs envolventes
-            e intuitivos, aprimorando a experiência do usuário.
-          </p>
-        </motion.li>
-
-        <motion.li
-          ref={liRef}
-          variants={{
-            hidden: { opacity: 0, scale: 0.25 },
-            visible: { opacity: 1, scale: 1 },
-          }}
-          initial="hidden"
-          aria-hidden="true"
-          animate={motionAnimation}
-          transition={{ duration: 0.75, delay: 0.5 }}
-          className="group flex flex-col justify-around rounded-xl bg-slate-800 p-5 min-h-[20rem] group"
-        >
-          <div className="flex items-center gap-7">
-            <PiArrowFatLinesUpBold className="text-6xl text-green-500 drop-shadow-3xl group-hover:scale-125 transition ease-in-out delay-150" />
-            <h3 className="text-2xl text-white strong font-semibold">
-              Trafego Pago
-            </h3>
-          </div>
-
-          <p className="mt-10 text-white">
-            Potencializamos a visibilidade online da sua empresa com estratégias
-            de tráfego pago, utilizando ferramentas de ponta como Google Ads,
-            Instagram e Facebook ads para impulsionar seu crescimento.
-          </p>
-        </motion.li>
-
-        <motion.li
-          ref={liRef}
-          variants={{
-            hidden: { opacity: 0, scale: 0.25 },
-            visible: { opacity: 1, scale: 1 },
-          }}
-          initial="hidden"
-          aria-hidden="true"
-          animate={motionAnimation}
-          transition={{ duration: 0.75, delay: 0.5 }}
-          className="group flex flex-col justify-around rounded-xl bg-slate-800  p-5 min-h-[20rem]"
-        >
-          <div className="flex items-center gap-7">
-            <BsBarChartLine className="text-6xl text-green-500 drop-shadow-3xl group-hover:scale-125 transition ease-in-out delay-150" />
-            <h3 className="text-2xl text-white strong font-semibold">
-              Trafego Orgânico
-            </h3>
-          </div>
-
-          <p className="mt-10 text-white">
-            Otimizamos sua presença online com estratégias de tráfego orgânico,
-            fortalecendo a visibilidade e a reputação da sua empresa.
-          </p>
-        </motion.li>
+        {CARDS_INFO.map((el) => (
+          <Card
+            key={el.title}
+            ref={liRef}
+            motionAnimation={motionAnimation}
+            info={el}
+          />
+        ))}
       </ul>
     </article>
+  )
+}
+
+const Card = ({
+  ref,
+  motionAnimation,
+  info,
+}: {
+  ref?: React.Ref<HTMLLIElement>
+  motionAnimation: AnimationControls
+  info: {
+    title: string
+    description: string
+    icon: IconType
+  }
+}) => {
+  return (
+    <motion.li
+      ref={ref}
+      variants={{
+        hidden: { opacity: 0, scale: 0.25 },
+        visible: { opacity: 1, scale: 1 },
+      }}
+      initial="hidden"
+      aria-hidden="true"
+      animate={motionAnimation}
+      transition={{ duration: 0.75, delay: 0.5 }}
+      className="group flex flex-col justify-around rounded-xl bg-slate-100  p-5 min-h-[20rem]"
+    >
+      <div className="flex items-center gap-7">
+        <info.icon className="text-6xl text-green-500 drop-shadow-3xl group-hover:scale-125 transition ease-in-out delay-150" />
+        <h3 className="text-2xl  strong font-semibold">{info.title}</h3>
+      </div>
+
+      <p className="mt-10 ">{info.description}</p>
+    </motion.li>
   )
 }
