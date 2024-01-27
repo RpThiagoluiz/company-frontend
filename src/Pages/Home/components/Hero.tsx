53f904b1c14fb113256563b68d96f38048c330d9
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2024-01-27 19:00:36 -0300

    refactor: adjust page heigh

--- src/Pages/Home/components/Hero.tsx
+++ src/Pages/Home/components/Hero.tsx
@@ -34,7 +34,7 @@ export function Hero() {
     <motion.section
       style={{ opacity, scale }}
       ref={targetRef}
-      className="flex flex-col md:py-20 py-2 items-center gap-5 h-screen"
+      className="flex flex-col md:py-10 py-2 items-center gap-5 h-full"
     >
       <img
         loading="lazy"
