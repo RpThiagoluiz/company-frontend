b95e55dc1c2f9d5b2ead55b7444a414078e4492b
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2024-01-28 21:49:41 -0300

    chore: footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,109 @@
+import heroImage from '../../images/TF/hero.png'
+
 export function Footer() {
   return (
-    <footer className="bg-zinc-900 h-[580px] flex items-center justify-center">
-      <h3 className="text-3xl md:text-5xl font-bold text-white">Footer</h3>
+    <footer className="bg-stone-100 grid grid-cols-2 min-h-[30svh] w-full">
+      <section className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3 bg-white p-12">
+        <div className="flex">
+          <img
+            src={heroImage}
+            className="h-8 me-3 drop-shadow-green"
+            alt="TF LOGO"
+          />
+          <span className="text-2xl font-semibold whitespace-nowrap">
+            TF Soluções
+          </span>
+        </div>
+        <div>
+          <h2 className="mb-6 text-sm font-semibold uppercase">Follow us</h2>
+          <ul className="text-gray-500 dark:text-gray-400 font-medium">
+            <li className="mb-4">
+              <a
+                href="https://github.com/themesberg/flowbite"
+                className="hover:underline "
+              >
+                Github
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://discord.gg/4eeurUVvTy"
+                className="hover:underline"
+              >
+                Discord
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h2 className="mb-6 text-sm font-semibold uppercase ">Legal</h2>
+          <ul className="text-gray-500 dark:text-gray-400 font-medium">
+            <li className="mb-4">
+              <a href="#" className="hover:underline">
+                Privacy Policy
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline">
+                Terms &amp; Conditions
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <section className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3 bg-zinc-700 p-12">
+        <div className="flex">
+          <img
+            src={heroImage}
+            className="h-8 me-3 drop-shadow-green"
+            alt="TF LOGO"
+          />
+          <span className="text-2xl font-semibold whitespace-nowrap">
+            TF Soluções
+          </span>
+        </div>
+        <div>
+          <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+            Follow us
+          </h2>
+          <ul className="text-gray-500 dark:text-gray-400 font-medium">
+            <li className="mb-4">
+              <a
+                href="https://github.com/themesberg/flowbite"
+                className="hover:underline "
+              >
+                Github
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://discord.gg/4eeurUVvTy"
+                className="hover:underline"
+              >
+                Discord
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+            Legal
+          </h2>
+          <ul className="text-gray-500 dark:text-gray-400 font-medium">
+            <li className="mb-4">
+              <a href="#" className="hover:underline">
+                Privacy Policy
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline">
+                Terms &amp; Conditions
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
     </footer>
   )
 }
