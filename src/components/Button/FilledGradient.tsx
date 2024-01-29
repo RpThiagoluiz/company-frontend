821c4a124e0c8a03ad10f355b9c8aa1bb14ac6aa
Rpthiagoluiz <rp.thiagoluiz@gmail.com>
2024-01-29 19:59:33 -0300

    chore: button filled border style

--- src/components/Button/FilledGradient.tsx
+++ src/components/Button/FilledGradient.tsx
@@ -9,7 +9,7 @@ export function FilledGradient({ children, extraStyles, ...rest }: Props) {
   return (
     <button
       type="button"
-      className={`text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-400/50 dark:shadow-lg dark:shadow-green-800/80 py-2 px-8 mr-auto text-center font-semibold rounded ${extraStyles}`}
+      className={`text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-400/50 dark:shadow-lg dark:shadow-green-800/80 py-2 px-8 mr-auto text-center font-semibold rounded-lg ${extraStyles}`}
       {...rest}
     >
       {children}
