3e06227dab5454b603474a2fef3b9984f50fb3b1
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2024-02-06 22:08:46 -0300

    feat: header component for about page

---
+++ src/Pages/About/components/Header.tsx
@@ -0,0 +1,13 @@
+export function Header() {
+  return (
+    <header className="h-full w-full clip-path-ellipse flex justify-center">
+      <div className="py-20 w-1/3">
+        <h1 className="text-center leading-10 tracking-wider text-3xl font-bold text-white">
+          Priorizamos produtos de qualidade excepcional que agregam valor real
+          aos nossos clientes, gerando resultados mensuráveis
+          <span className="text-green-600 font-bold text-3xl">.</span>
+        </h1>
+      </div>
+    </header>
+  )
+}
