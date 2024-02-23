a67faf35884d613f5646ede631cfc7a8aa986b17
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2024-02-22 22:46:18 -0300

    feat: register routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Navbar } from '@components/Navbar'
 import { routes } from './registerRoutes'
-
 import { Footer } from '@/components/Footer'
 import { ScrollToTop } from '@/hooks/useScrollToTop'
+import { NotFound } from '@/Pages/NotFound'
+
 export function AppRoutes() {
   return (
     <BrowserRouter>
@@ -13,9 +14,14 @@ export function AppRoutes() {
       <Routes>
         {React.Children.toArray(
           routes.map((route) => (
-            <Route path={route.path} element={route.component} />
+            <Route
+              key={route.path}
+              path={route.path}
+              element={route.component}
+            />
           )),
         )}
+        <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
     </BrowserRouter>
--- src/routes/registerRoutes.tsx
+++ src/routes/registerRoutes.tsx
@@ -1,9 +1,19 @@
 import { About } from '@/Pages/About'
 import { Contact } from '@/Pages/Contact'
 import { Home } from '@/Pages/Home'
+import { InformationSecurity } from '@/Pages/InformationSecurity'
+import { PrivatePolicy } from '@/Pages/PrivatePolicy'
 import { Services } from '@/Pages/Services'
+import { TermsAndConditions } from '@/Pages/TermsAndConditions'
 
-export type Routes = '/' | '/about' | '/services' | '/contact'
+export type Routes =
+  | '/'
+  | '/about'
+  | '/services'
+  | '/contact'
+  | '/private-policy'
+  | '/information-security'
+  | '/terms-conditions'
 
 export const routes: {
   path: Routes
@@ -13,4 +23,8 @@ export const routes: {
   { path: '/about', component: <About /> },
   { path: '/services', component: <Services /> },
   { path: '/contact', component: <Contact /> },
+  { path: '/contact', component: <Contact /> },
+  { path: '/private-policy', component: <PrivatePolicy /> },
+  { path: '/information-security', component: <InformationSecurity /> },
+  { path: '/terms-conditions', component: <TermsAndConditions /> },
 ]
